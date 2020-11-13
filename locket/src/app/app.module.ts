7b3520f0b3c443ae28f4220802942a1e286487dd
Badr <badrkmc@gmail.com>
2020-11-13 22:32:51 +0500

    add locket component

--- locket/src/app/app.module.ts
+++ locket/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { LocketComponent } from './locket/locket.component';
+
+import { LocketService } from './services/locket.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LocketComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    LocketService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
