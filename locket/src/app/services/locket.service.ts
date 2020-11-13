d7c32b0f21b01633d028cf829b272301e2eb6957
Badr <badrkmc@gmail.com>
2020-11-13 23:09:04 +0500

    make method for basic communication with backend

--- locket/src/app/services/locket.service.ts
+++ locket/src/app/services/locket.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+import { HttpProcessingService } from '../services/http-processing.service';
+import { serverURL } from '../util/serverURL';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocketService {
 
-  constructor() { }
+  constructor(private client: HttpClient, private httpProessingService: HttpProcessingService) { }
+
+  public communicate(): Observable<Object> {
+    return this.client.get<Object>(serverURL + 'communicate')
+      .pipe(
+        catchError(this.httpProessingService.handleError));
+  }
+
 }
