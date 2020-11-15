fb1171170a8f618dddcc14dad48db5091c8b9e1a
Badr <badrkmc@gmail.com>
2020-11-15 16:12:06 +0500

    fixed multer, files now get saved to storage

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,24 +5,55 @@ app.use(express.json())
 import cors from "cors";
 app.use(cors());
 
-const PATH = './';
-import fileUpload from "express-fileupload";
-app.use(fileUpload())
+import multer from "multer";
+const storage = multer.diskStorage({
+    destination: (req: Request, file, cb) => {
+        cb(null, './')
+    },
+    filename: (req: Request, file, cb) => {
+        cb(null, 'image')
+    }
+})
+
+var upload = multer({
+    storage
+})
 
 app.get("/communicate", (req: Request, res: Response) => {
+
     console.log("Hit the endpoint!");
     res.status(200).json({
         received: true,
     });
 })
 
-app.post("/upload", (req: Request, res: Response) => {
-    console.log(req.files)
+app.post("/uploadFile", upload.single('file'), (req: Request, res: Response) => {
+
+    console.log("Uploading single file...")
+
+    const file = req.file
+    console.log(file.filename)
+    console.log(file.size)
+
     res.status(200).json({
         received: true,
+        file
     })
 })
 
+
+app.post("/uploadFiles", upload.array('files'), (req: Request, res: Response) => {
+
+    console.log("Uploading multiple files...")
+
+    const files = req.files
+    console.log(files)
+
+    res.status(200).json({
+        received: true,
+        files
+    })
+})
 app.listen(3000, () => {
     console.log('server started at port 3000...')
 })
