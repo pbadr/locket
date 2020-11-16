d775ae112832d8ef20d76727a0742353a5ce3785
Badr <badrkmc@gmail.com>
2020-11-16 19:50:46 +0500

    specify types for encryption

--- server/src/util/encryption.ts
+++ server/src/util/encryption.ts
@@ -14,10 +14,10 @@ import crypto from "crypto";
 
 export function textEncryption(algorithm: string, text: string): EncryptionObject {
 
-    const secret = crypto.scryptSync(process.env.SECRET || '', 'salt', 24);
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv(algorithm, secret, iv)
-    const encryptedText = cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
+    const secret: Buffer = crypto.scryptSync(process.env.SECRET || '', 'salt', 24);
+    const iv: Buffer = crypto.randomBytes(16);
+    const cipher: crypto.Cipher = crypto.createCipheriv(algorithm, secret, iv)
+    const encryptedText: string = cipher.update(text, 'utf8', 'hex') + cipher.final('hex');
 
     return {
         encryptedText,
@@ -26,9 +26,9 @@ export function textEncryption(algorithm: string, text: string): EncryptionObjec
 }
 
 export function textDecrypt(algorithm: string, encryption: string, iv: Buffer): string {
-    const secret = crypto.scryptSync(process.env.SECRET || '', 'salt', 24);
-    const decipher = crypto.createDecipheriv(algorithm, secret, iv)
-    const decryptedText = decipher.update(encryption, 'hex', 'utf8') + decipher.final('utf8');
+    const secret: Buffer = crypto.scryptSync(process.env.SECRET || '', 'salt', 24);
+    const decipher: crypto.Decipher = crypto.createDecipheriv(algorithm, secret, iv)
+    const decryptedText: string = decipher.update(encryption, 'hex', 'utf8') + decipher.final('utf8');
 
     return decryptedText
 }
