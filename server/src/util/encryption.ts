86c99083ef491d57a62c9d0a67ff23e7f496fd28
Badr <badrkmc@gmail.com>
2020-11-25 11:51:21 +0500

    changed from piping to Buffer.concat

--- server/src/util/encryption.ts
+++ server/src/util/encryption.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 import fs from "fs";
 
-import { PATH_TO_UPLOAD_WITH_NAME, PATH_TO_UPLOAD, readBuffer, deleteFile } from "./file";
+import { PATH_TO_UPLOAD_WITH_NAME, PATH_TO_UPLOAD, readFileBuffer } from "./file";
 import FileType, { FileTypeResult } from 'file-type';
 
 dotenv.config({
@@ -17,7 +17,7 @@ interface EncryptionObject {
 
 import crypto from "crypto";
 
-export function textEncryption(algorithm: string, text: string): EncryptionObject {
+export function textEncrypt(algorithm: string, text: string): EncryptionObject {
 
     const secret: Buffer = fetchKey(24);
     const iv: Buffer = generateIV(16);
@@ -26,7 +26,7 @@ export function textEncryption(algorithm: string, text: string): EncryptionObjec
 
     return {
         encryptedText,
-        iv
+        iv,
     }
 }
 
@@ -41,62 +41,54 @@ export function textDecrypt(algorithm: string, encryption: string, iv: Buffer): 
 export function encryptFileToDisk(pathToFile: string): any {
     const secret: Buffer = fetchKey(32);
     const iv: Buffer = generateIV(16);
+    const cipher = crypto.createCipheriv("aes-256-cbc", secret, iv);
 
-    const cipher = crypto.createCipheriv("aes-256-cbc", secret, iv)
-
-    const PATH_TO_UPLOAD_WITH_NAME_ENCRYPTED = '' + PATH_TO_UPLOAD + 'encrypted/encrypted'
-
-    const input = fs.createReadStream(pathToFile);
-    console.log("READING FILE BYTES BEFORE ENCRYPTION:");
-    readBuffer(pathToFile);
-
+    const PATH_TO_UPLOAD_WITH_NAME_ENCRYPTED = PATH_TO_UPLOAD + 'encrypted/encrypted';
     try {
-        const output = fs.createWriteStream(PATH_TO_UPLOAD_WITH_NAME_ENCRYPTED + '.enc')
+        const encryptedFile = fs.createWriteStream(PATH_TO_UPLOAD_WITH_NAME_ENCRYPTED + '.enc');
 
-        input.pipe(cipher)
-            .pipe(output);
+        var data = fs.readFileSync(pathToFile);
 
-        output.on('finish', () => {
-            console.log('File encrypted successfully...');
-            readBuffer(PATH_TO_UPLOAD_WITH_NAME_ENCRYPTED + '.enc');
-        });
+        const encrypted = Buffer.concat([
+            cipher.update(data),
+            cipher.final()
+        ])
+        encryptedFile.write(encrypted);
+
     } catch (err) {
         console.log(err);
     }
 
     return {
         pathToEncryptedFile: PATH_TO_UPLOAD_WITH_NAME_ENCRYPTED + '.enc',
-        iv
+        iv,
     }
 }
 
 export function decryptFileToDisk(pathToFile: string, iv: Buffer): void {
 
-    let dataEncryptionBuffer: Buffer;
-
     const secret: Buffer = fetchKey(32);
     const decipher = crypto.createDecipheriv("aes-256-cbc", secret, iv);
 
-    fs.readFile(pathToFile, async (err, data) => {
-        if (err) throw err
+    fs.readFile(pathToFile, async (err, encryptedDataBuffer) => {
+        if (err) {
+            console.log("decryptFileToDisk: CANNOT READ FILE:");
+            throw err;
+        }
 
-        dataEncryptionBuffer = data;
+        const decryptedFile = Buffer.concat([decipher.update(encryptedDataBuffer), decipher.final()]);
 
-        const output = Buffer.concat([decipher.update(dataEncryptionBuffer), decipher.final()]);
-
-        FileType.fromBuffer(output)
+        FileType.fromBuffer(decryptedFile)
             .then(fileObject => {
+
                 var fileType: FileTypeResult | undefined = fileObject
-                fs.writeFileSync(PATH_TO_UPLOAD + 'decrypted.' + fileType?.ext, output);
-                console.log("Successfully written decrypted file to disk!");
+                fs.writeFileSync(PATH_TO_UPLOAD + 'decrypted.' + fileType?.ext, decryptedFile);
+                console.log("decryptFileToDisk: Successfully written decrypted file to disk!");
             })
             .catch(err => {
-                console.log("Error getting file type from encrypted file! - ", err);
+                console.log("decryptFileToDisk: Error getting file type from encrypted file! - ", err);
             });
-
-
     })
-
 }
 
 function fetchKey(bytes: number): Buffer {
