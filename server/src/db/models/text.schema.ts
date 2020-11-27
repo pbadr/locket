bd2328e54894bdecfb3c4823c06f7d37dce74ba9
Badr <badrkmc@gmail.com>
2020-11-27 15:25:57 +0500

    make a model for text

---
+++ server/src/db/models/text.schema.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+export default mongoose.model(
+
+    "Text",
+
+    new Schema({
+        text: {
+            type: Schema.Types.String,
+            required: true,
+        },
+        textBytes: {
+            type: Schema.Types.Buffer,
+            required: true,
+        },
+        summary: {
+            type: Schema.Types.String,
+        },
+        date: {
+            type: Schema.Types.Date,
+            default: Date.now,
+        }
+    })
+
+);
